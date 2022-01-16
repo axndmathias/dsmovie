5a75dbf66c00ba9892055d0051d398f3d61797c3
Alexandre Mathias <mathias.alexandre@gmail.com>
2022-01-16 09:24:56 +0100

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,19 +1,36 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movies";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-   // forma errada
-  // axios.get('https://axnd-dsmovie.herokuapp.com/movies?size=12&page=0')
-  axios.get(`${BASE_URL}/movies?size=12&page=0`)
+
+  const [pageNumber, setPageNumber] = useState(0);
+
+ 
+
+  useEffect(()=>{
+    axios.get(`${BASE_URL}/movies?size=12&page=2`)
     .then(response => {
-      console.log(response.data);
+      const data = response.data as MoviePage;
+      console.log(data);
+          setPageNumber(data.number);
     });
+  }, []);
+
+   // forma errada
+  //  axios.get('https://axnd-dsmovie.herokuapp.com/movies?size=12&page=0')
+  // axios.get(`${BASE_URL}/movies?size=12&page=0`)
+  //   .then(response => {
+  //     console.log(response.data);
+  //   });
 
   return (
     <>
+    <p>{pageNumber}</p>
       <Pagination />
       <div className="container">
         <div className="row">
